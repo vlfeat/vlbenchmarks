f6f65a0b04cfe917328376d81276aeef3a65c2f4
Varun Gulshan <varungulshan@gmail.com>
2012-03-16 22:55:32 +0000

    Documentation improved

--- +affineDetectors/benchmarkDemo.m
+++ +affineDetectors/benchmarkDemo.m
@@ -1,6 +1,18 @@
 function benchMarkDemo()
-% Script to run benchmarks for different algorithms
-% Making it a function, so that variables don't clutter workspace
+% BENCHMARKDEMO Script demonstrating how to run the benchmarks for
+%   different algorithms.
+%
+%   The following datasets are supported right now:
+%     <a href="matlab: help affineDetectors.vggDataset">affineDetectors.vggDataset</a>
+%
+%   The following detectors are supported right now:
+%     <a href="matlab: help affineDetectors.vlFeatDOG">affineDetectors.vlFeatDOG</a>
+%     <a href="matlab: help affineDetectors.vlFeatMser">affineDetectors.vlFeatMser</a>
+%     <a href="matlab: help affineDetectors.vggAffine">affineDetectors.vggAffine</a>
+%     <a href="matlab: help affineDetectors.vggMser">affineDetectors.vggMser</a>
+%     <a href="matlab: help affineDetectors.sfop">affineDetectors.sfop</a>
+%
+%   See <a href="matlab: help affineDetectors.exampleDetector">affineDetectors.exampleDetector</a> on how to add your own detector
 
 detectors{1} = affineDetectors.vlFeatDOG(); % Default options
 detectors{2} = affineDetectors.vggMser('ms',300); % Custom options
@@ -8,8 +20,12 @@ detectors{3} = affineDetectors.vlFeatMser('minarea',0.001); % Custom options
 detectors{3}.detectorName = 'MSER(VLfeat)'; % You can change the name that is
 % used for displaying results from the default also.
 
-% See affineDetectors.exampleDetector on how to add your own detector
+%detectors{1} = affineDetectors.vggAffine('detector','hessian');
+%detectors{2} = affineDetectors.vggAffine('detector','harris');
+%detectors{3} = affineDetectors.vggMser();
+
 
 dataset = affineDetectors.vggDataset('category','graf');
 
-affineDetectors.runBenchmark(detectors,dataset,'verifyKristian','true');
+%affineDetectors.runBenchmark(detectors,dataset,'verifyKristian',true);
+affineDetectors.runBenchmark(detectors,dataset);
