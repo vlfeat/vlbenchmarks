3fef711b009685fc2e0e0058d2210dd1137f1cc3
Andrea Vedaldi <vedaldi@gmail.com>
2012-09-03 19:44:55 +0100

    computeEllipseOverlap.m: cleanup

--- +benchmarks/+helpers/computeEllipseOverlap.m
+++ +benchmarks/+helpers/computeEllipseOverlap.m
@@ -1,13 +1,11 @@
 function overlap =  computeEllipseOverlap(vggA,vggB)
-% COMPUTEELLIPSEOVERLAP Computes ellipse overlap 
-%
-%  OVERLAP = computeEllipseOverlap(VGG_FRAMEA, VGG_FRAMEB) calls
-%    Kristian Mikolajczyk Ellipse overlap function and returns overlap
-%    ratio of the ellipses VGG_FRAMEA and VGG_FRAMEB.
-%
+% COMPUTEELLIPSEOVERLAP Computes ellipse overlap
+%   OVERLAP = COMPITEELLIPSEOVERLAP(VGGA, VGGB) calls Kristian
+%   Mikolajczyk ellipse overlap function and returns overlap ratio of
+%   the elliptical frames ELL1 and ELL2.
+
 import benchmarks.*
 
-% 
 [w, tw, d, td] = helpers.mexComputeEllipseOverlap(vggA, vggB, -1);
 
 overlap = 1 - tw / 100 ;
