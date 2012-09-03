6c5021b990bd13841b055b705e7a66a17939baf1
Andrea Vedaldi <vedaldi@gmail.com>
2012-09-03 18:21:21 +0100

    fastEllipseOverlap.m: cleanup

--- +benchmarks/+helpers/fastEllipseOverlap.m
+++ +benchmarks/+helpers/fastEllipseOverlap.m
@@ -1,35 +1,39 @@
-function result = fastEllipseOverlaps(f1, f2, varargin)
+function result = fastEllipseOverlap(f1, f2, varargin)
 % FASTELLIPSEOVERLAP Compute overlaps of two sets ov ellipses
-%  EVAL = MATCHELLIPSES(F1, F2) Computes ellipse overlaps between 
-%  each ellipses from F1 to each ellipse in F2.
-%  EVAL output structure of size(F2,2) contains following values:
-%  
-%    EVAL.NEIGH : for each F2 frame, the list of F1 neighbors
-%    EVAL.SCORES : for each F2 frame, relative overlap of F1 neighbours
+%   EVAL = FASTELLIPSEOVERLAP(F1, F2) computes the overlap scores
+%   beteen all pairs of ellipses in F1 and F2.  EVAL output structure
+%   of size(F2,2) contains following values:
 %
-%  When frame scale normalisation is not applied the function is 
-%  symmetric. With rescaling, the frames of F2 are used to fix the 
-%  scaling factors.
+%    EVAL.NEIGH::
+%      The list of neighbours in F1 for each ellipse in F1.
 %
-%  MATCHELLIPSES(F1, F2, 'OptionName', OptionValue) accepts the
-%  following options:
+%    EVAL.SCORES::
+%      The correpsonding overlaps.
 %
-%  NormaliseFrames :: [true]
-%    Fix the the frames scale so that each F2 frame has got scale 
-%    defined by the 'NormalisedScale' option value.
+%   When frame scale normalisation is not applied the function is
+%   symmetric. With rescaling, the frames of F2 are used to fix the
+%   scaling factors.
 %
-%   NormalisedScale :: [30]   
-%     When frames scale normalisation applied, fixed scale of frames
-%     in F2.
-% 
-%   minAreaRatio :: [0.3]
-%     Precise ellipse overlap is calculated only for ellipses E1 
-%     and E2 which area ratio is bigger than 'minAreaRatio', s.t.:
+%   MATCHELLIPSES(F1, F2, 'OptionName', OptionValue) accepts the
+%   following options:
 %
-%     area(E1)/area(E2) > minAreaRatio, area(E1) < area(E2)
-%     
+%   NormaliseFrames:: [true]
+%     Fix the the frames scale so that each F2 frame has got scale
+%     defined by the 'NormalisedScale' option value.
 %
-%  Author:: Andrea Vedaldi
+%   NormalisedScale:: [30]
+%      When frames scale normalisation applied, fixed scale of frames
+%      in F2.
+%
+%   MinAreaRatio:: [0.3]
+%      Precise ellipse overlap is calculated only for ellipses E1
+%      and E2 which area ratio is bigger than 'minAreaRatio', s.t.:
+%
+%        area(E1)/area(E2) > minAreaRatio, area(E1) < area(E2)
+
+%  Author:: Andrea Vedaldi, Karel Lenc
+
+% AUTORIGHTS
 
   import benchmarks.*;
 
@@ -65,7 +69,7 @@ function result = fastEllipseOverlaps(f1, f2, varargin)
     canOverlap = sqrt(vl_alldist2(f2(1:2, i2), f1(1:2,:))) < 4 * sqrt(a2(i2) / pi);
     % a2(i2) - area of tested ellipse
     % a1 - areas of all ellipses
-    % min(a2(i2) a1) - vector 
+    % min(a2(i2) a1) - vector
     maxOverlap = min(a2(i2), a1) ./ max(a2(i2), a1) .* canOverlap ;
     neighs{i2} = find(maxOverlap > conf.minAreaRatio) ;
 
