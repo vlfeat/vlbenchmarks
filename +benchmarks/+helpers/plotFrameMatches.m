45e528ad89b44bf9f5317091ee710fd0616fd9e0
Karel Lenc <karel@lenc.info>
2012-09-15 18:38:47 +0200

    Changed the plotFrameMatches helper. Now it is more versatile.

--- +benchmarks/+helpers/plotFrameMatches.m
+++ +benchmarks/+helpers/plotFrameMatches.m
@@ -1,65 +1,104 @@
-function plotFrameMatches(bestMatches, reprojectedFrames,...
-  imageAPath, figA, imageBPath, figB)
+function plotFrameMatches(bestMatches,reprojFrames,varargin)
 % PLOTFRAMEMATCHES Visualise matched frames in original images.
-%   plotFramesMatches(BEST_MATCHES, REPROJ_FRAMES, IMAGE_A_PATH, 
-%   FIG_A_NUM) Plots matches in the reference image IMAGE_A_PATH in 
-%   figure FIG_A between frames stored in cell array REPROJ_FRAMES 
-%   with the following contents:
+%   PLOTFRAMEMATCHES(BEST_MATCHES, REPROJ_FRAMES) 
+%     Plots matches between frames stored in cell array REPROJ_FRAMES with
+%     the following contents:
 %
-%     {FRAMES_A,FRAMES_B,REPROJ_FRAMES_A,REPROJ_FRAMES_B}
+%     REPROJ_FRAMES = {FRAMES_A,FRAMES_B,REPROJ_FRAMES_A,REPROJ_FRAMES_B}
 %
-%   to figure FIG_A with image def. by IMAGE_A_PATH as a background.
-%   BEST_MATCHES is an matrix of size [1 size(framesA,2)] where
-%   value BEST_MATCHES(iFrameA) is an index of matched frame in
-%   FRAMES_B of FRAMES_A(:,iFrameA) or zero if frame is not matched.
-%   Matched ellipses are visualised with lines connecting their
-%   centres.
+%     BEST_MATCHES is an matrix of size [1 size(framesA,2)] where
+%     value BEST_MATCHES(iFrameA) is an index of matched frame in
+%     FRAMES_B of FRAMES_A(:,iFrameA) or zero if frame is not matched.
+%     Matched ellipses are visualised with lines connecting their
+%     centres when 'PlotMatchLine' is true.
 %
-%   plotFramesMatches(BEST_MATCHES, REPROJ_FRAMES, IMAGE_A_PATH, 
-%   FIG_A_NUM, IMAGE_B_PATH, FIG_B) Plots matched frames both in
-%   reference and projected image.
+%   PLOTFRAMEMATCHES(...,'OptionName',OptionValue) 
+%     Specify further options.
 %
-  
-  matchLineStyle = 'bx-';
-                        
-  imageA = imread(imageAPath);
+%   Available options:
+%
+%   IsReferenceImage:: true
+%     When true, plot matches as in the reference image. When false, plot
+%     as when in the tested image (using reprojected frames).
+%
+%   PlotLegend:: true
+%     Plot legend.
+%
+%   PlotMatchLine:: true
+%     Plot line between the centres of the matched frames.
+%
+%   MatchLineStyle:: {'bx-'}
+%     Style of the match line. Cell array passed to plot function.
+%
+%   RefImMatchedStyle:: {'Color',[0.1 0.1 0.9],'LineWidth',2}
+%     Style of the matched frames from the reference image. Cell array
+%     passed to vl_plotframe function.
+%
+%   RefImUnmatchedStyle:: {'Color',[0.1 0.1 0.4],'LineWidth',1}
+%     Style of the unmatched frames from the reference image. Cell array
+%     passed to vl_plotframe function.
+%
+%   TestImMatchedStyle:: {'Color',[0.1 0.9 0.1],'LineWidth',2};
+%     Style of the matched frames from the tested image. Cell array
+%     passed to vl_plotframe function.
+%
+%   TestImUnmatchedStyle:: {'Color',[0.1 0.4 0.1],'LineWidth',1};
+%     Style of the unmatched frames from the tested image. Cell array
+%     passed to vl_plotframe function.
+%
+% See also: plot, helpers.vl_plotframe
 
-  [framesA,framesB,reprojFramesA,reprojFramesB] = reprojectedFrames{:};
+% AUTORIGHTS
 
-  figure(figA); imshow(imageA); colormap gray; hold on ; 
-  aF = vl_plotframe(framesA,'linewidth', 1);
-  % Plot the transformed and matched frames from B on A in blue
-  matchedBFrames = bestMatches(1,(bestMatches(1,:)~=0));
-  matchedAFrames = find(bestMatches(1,:)~=0);
-  % Plot the remaining frames from B on A in red
-  unmatchedBFrames = setdiff(1:size(framesB,2),matchedBFrames);
-  uBF = vl_plotframe(reprojFramesB(:,unmatchedBFrames),'r','linewidth',1);
-  mBF = vl_plotframe(reprojFramesB(:,matchedBFrames),'b','linewidth',1);
+opts.refImMatchedStyle = {'Color',[0.1 0.1 0.9],'LineWidth',2};
+opts.refImUnmatchedStyle = {'Color',[0.1 0.1 0.4],'LineWidth',1};
+opts.testImMatchedStyle = {'Color',[0.1 0.9 0.1],'LineWidth',2};
+opts.testImUnmatchedStyle = {'Color',[0.1 0.4 0.1],'LineWidth',1};
+opts.plotMatchLine = true;
+opts.matchLineStyle = {'bx-'};
+opts.isReferenceImage = true;
+opts.plotLegend = true;
+opts = helpers.vl_argparse(opts, varargin);
+
+[framesA,framesB,reprojFramesA,reprojFramesB] = reprojFrames{:};
+hold on;
+
+matchBFrames = bestMatches(1,(bestMatches(1,:)~=0));
+matchAFrames = find(bestMatches(1,:)~=0);
+unmatchAFrames = setdiff(1:size(framesA,2),matchAFrames);
+unmatchBFrames = setdiff(1:size(framesB,2),matchBFrames);
+
+if opts.isReferenceImage
+  uBF = vl_plotframe(reprojFramesB(:,unmatchBFrames),opts.testImUnmatchedStyle{:});
+  uAF = vl_plotframe(framesA(:,unmatchAFrames),opts.refImUnmatchedStyle{:});
+  mBF = vl_plotframe(reprojFramesB(:,matchBFrames),opts.testImMatchedStyle{:});
+  mAF = vl_plotframe(framesA(:,matchAFrames),opts.refImMatchedStyle{:});
   % Plot the connecting lines between matches
-  for i=1:numel(matchedAFrames)
-    plot([framesA(1,matchedAFrames(i))' reprojFramesB(1,matchedBFrames(i))'],...
-      [framesA(2,matchedAFrames(i))' reprojFramesB(2,matchedBFrames(i))'],...
-      matchLineStyle);
-  end
-  title('Reference image detections');
-  legend([aF mBF uBF],'Det. frames in Ref. Image', ... 
-    'Matched transf. image frames','Unmatched transf. image frames');
-
-  if nargin > 4
-    imageB = imread(imageBPath);
-    figure(figB); imshow(imageB);  colormap gray; hold on ; 
-    bF = vl_plotframe(framesB,'linewidth', 1);
-    unmatchedAFrames = setdiff(1:size(framesA,2),matchedAFrames);
-    uAF = vl_plotframe(reprojFramesA(:,unmatchedAFrames),'r','linewidth',1);
-    % Plot the transformed and matched frames from A on B in blue
-    mAF = vl_plotframe(reprojFramesA(:,matchedAFrames), 'b', 'linewidth', 1);
-    for i=1:numel(matchedAFrames)
-      plot([reprojFramesA(1,matchedAFrames(i))' framesB(1,matchedBFrames(i))'],...
-        [reprojFramesA(2,matchedAFrames(i))' framesB(2,matchedBFrames(i))'],...
-        matchLineStyle);
+  if opts.plotMatchLine
+    for i=1:numel(matchAFrames)
+      plot([framesA(1,matchAFrames(i))' reprojFramesB(1,matchBFrames(i))'],...
+        [framesA(2,matchAFrames(i))' reprojFramesB(2,matchBFrames(i))'],...
+        opts.matchLineStyle{:});
     end
-    title('Transformed image detections');
-    legend([bF mAF uAF],'Det. frames in Transf. Image', ... 
-    'Matched ref. image frames','Unmatched ref. image frames');
   end
+else
+  % Plot the matches in the tested image
+  uAF = vl_plotframe(reprojFramesA(:,unmatchAFrames),opts.refImUnmatchedStyle{:});
+  uBF = vl_plotframe(framesB(:,unmatchBFrames),opts.testImUnmatchedStyle{:});
+  mAF = vl_plotframe(reprojFramesA(:,matchAFrames),opts.refImMatchedStyle{:});
+  mBF = vl_plotframe(framesB(:,matchBFrames),opts.testImMatchedStyle{:});
+  if opts.plotMatchLine
+    for i=1:numel(matchAFrames)
+      plot([reprojFramesA(1,matchAFrames(i))' framesB(1,matchBFrames(i))'],...
+        [reprojFramesA(2,matchAFrames(i))' framesB(2,matchBFrames(i))'],...
+        opts.matchLineStyle{:});
+    end
+  end
+end
+
+if opts.plotLegend
+  legend([mAF uAF mBF uBF],'Matched ref. image frames', ...
+    'Unmatched ref. image frames', 'Matched test image frames',...
+    'Unmatched test image frames');
+end
 end
