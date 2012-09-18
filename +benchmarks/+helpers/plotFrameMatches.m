d9da127c3830ed801918bdd9784a2718b585a09b
Karel Lenc <karel@lenc.info>
2012-09-18 11:51:35 +0200

    Improved plotFrameMatches.

--- +benchmarks/+helpers/plotFrameMatches.m
+++ +benchmarks/+helpers/plotFrameMatches.m
@@ -21,41 +21,26 @@ function plotFrameMatches(bestMatches,reprojFrames,varargin)
 %     When true, plot matches as in the reference image. When false, plot
 %     as when in the tested image (using reprojected frames).
 %
+%   PlotUnmatched:: true;
+%
 %   PlotLegend:: true
 %     Plot legend.
 %
-%   PlotMatchLine:: true
+%   PlotMatchLine:: false
 %     Plot line between the centres of the matched frames.
-%
-%   MatchLineStyle:: {'bx-'}
-%     Style of the match line. Cell array passed to plot function.
-%
-%   RefImMatchedStyle:: {'Color',[0.1 0.1 0.9],'LineWidth',2}
-%     Style of the matched frames from the reference image. Cell array
-%     passed to vl_plotframe function.
-%
-%   RefImUnmatchedStyle:: {'Color',[0.1 0.1 0.4],'LineWidth',1}
-%     Style of the unmatched frames from the reference image. Cell array
-%     passed to vl_plotframe function.
-%
-%   TestImMatchedStyle:: {'Color',[0.1 0.9 0.1],'LineWidth',2};
-%     Style of the matched frames from the tested image. Cell array
-%     passed to vl_plotframe function.
-%
-%   TestImUnmatchedStyle:: {'Color',[0.1 0.4 0.1],'LineWidth',1};
-%     Style of the unmatched frames from the tested image. Cell array
-%     passed to vl_plotframe function.
 %
 % See also: plot, helpers.vl_plotframe
 
 % AUTORIGHTS
 
-opts.refImMatchedStyle = {'Color',[0.1 0.1 0.9],'LineWidth',2};
-opts.refImUnmatchedStyle = {'Color',[0.1 0.1 0.4],'LineWidth',1};
-opts.testImMatchedStyle = {'Color',[0.1 0.9 0.1],'LineWidth',2};
-opts.testImUnmatchedStyle = {'Color',[0.1 0.4 0.1],'LineWidth',1};
-opts.plotMatchLine = true;
-opts.matchLineStyle = {'bx-'};
+refImMatchedStyle = {'Color',[0.1 0.1 0.9],'LineWidth',2};
+refImUnmatchedStyle = {'Color',[0.2 0.2 0.5],'LineWidth',1};
+testImMatchedStyle = {'Color',[0.1 0.9 0.1],'LineWidth',2};
+testImUnmatchedStyle = {'Color',[0.2 0.5 0.2],'LineWidth',1};
+matchedBgFramesStyle = {'k','LineWidth',4};
+matchLineStyle = {'bx-'};
+opts.plotUnmatched = true;
+opts.plotMatchLine = false;
 opts.isReferenceImage = true;
 opts.plotLegend = true;
 opts = helpers.vl_argparse(opts, varargin);
@@ -69,36 +54,36 @@ unmatchAFrames = setdiff(1:size(framesA,2),matchAFrames);
 unmatchBFrames = setdiff(1:size(framesB,2),matchBFrames);
 
 if opts.isReferenceImage
-  uBF = vl_plotframe(reprojFramesB(:,unmatchBFrames),opts.testImUnmatchedStyle{:});
-  uAF = vl_plotframe(framesA(:,unmatchAFrames),opts.refImUnmatchedStyle{:});
-  mBF = vl_plotframe(reprojFramesB(:,matchBFrames),opts.testImMatchedStyle{:});
-  mAF = vl_plotframe(framesA(:,matchAFrames),opts.refImMatchedStyle{:});
-  % Plot the connecting lines between matches
-  if opts.plotMatchLine
-    for i=1:numel(matchAFrames)
-      plot([framesA(1,matchAFrames(i))' reprojFramesB(1,matchBFrames(i))'],...
-        [framesA(2,matchAFrames(i))' reprojFramesB(2,matchBFrames(i))'],...
-        opts.matchLineStyle{:});
-    end
-  end
+  framesB = reprojFramesB;
 else
-  % Plot the matches in the tested image
-  uAF = vl_plotframe(reprojFramesA(:,unmatchAFrames),opts.refImUnmatchedStyle{:});
-  uBF = vl_plotframe(framesB(:,unmatchBFrames),opts.testImUnmatchedStyle{:});
-  mAF = vl_plotframe(reprojFramesA(:,matchAFrames),opts.refImMatchedStyle{:});
-  mBF = vl_plotframe(framesB(:,matchBFrames),opts.testImMatchedStyle{:});
-  if opts.plotMatchLine
-    for i=1:numel(matchAFrames)
-      plot([reprojFramesA(1,matchAFrames(i))' framesB(1,matchBFrames(i))'],...
-        [reprojFramesA(2,matchAFrames(i))' framesB(2,matchBFrames(i))'],...
-        opts.matchLineStyle{:});
-    end
+  framesA = reprojFramesA;
+end
+
+% Plot the matches in the tested image
+if opts.plotUnmatched
+  uAF = vl_plotframe(framesA(:,unmatchAFrames),refImUnmatchedStyle{:});
+  uBF = vl_plotframe(framesB(:,unmatchBFrames),testImUnmatchedStyle{:});
+end
+vl_plotframe(framesA(:,matchAFrames),matchedBgFramesStyle{:});
+mAF = vl_plotframe(framesA(:,matchAFrames),refImMatchedStyle{:});
+vl_plotframe(framesB(:,matchBFrames),matchedBgFramesStyle{:});
+mBF = vl_plotframe(framesB(:,matchBFrames),testImMatchedStyle{:});
+if opts.plotMatchLine
+  for i=1:numel(matchAFrames)
+    plot([framesA(1,matchAFrames(i))' framesB(1,matchBFrames(i))'],...
+      [framesA(2,matchAFrames(i))' framesB(2,matchBFrames(i))'],...
+      matchLineStyle{:});
   end
 end
 
 if opts.plotLegend
-  legend([mAF uAF mBF uBF],'Matched ref. image frames', ...
-    'Unmatched ref. image frames', 'Matched test image frames',...
-    'Unmatched test image frames');
+  if opts.plotUnmatched
+    legend([mAF uAF mBF uBF],'Matched ref. image frames', ...
+      'Unmatched ref. image frames', 'Matched test image frames',...
+      'Unmatched test image frames');
+  else
+    legend([mAF mBF],'Matched ref. image frames', ...
+      'Matched test image frames');
+  end
 end
 end
