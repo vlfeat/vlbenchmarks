05d7a20a08e15651ae481c1767f1be16979d72a5
Karel Lenc <karel@lenc.info>
2012-09-21 18:41:48 +0200

    Added template feature detector wrapper.

---
+++ +localFeatures/TemplateWrapper.m
@@ -0,0 +1,61 @@
+classdef TemplateWrapper < localFeatures.GenericLocalFeatureExtractor
+% localFeatures.TemplateWrapper Feature detector wrapper template
+%   Skeleton of image feature detector wrapper.
+%
+% See also: localFeatures.ExampleLocalFeatureExtractor
+
+% Authors: Karel Lenc
+
+% AUTORIGHTS
+  properties (SetAccess=private, GetAccess=public)
+    % Detector options
+    Opts = struct(...
+      'OptionName','OptionValue'... % A detector option
+      );
+  end
+
+  methods
+    function obj = ExampleLocalFeatureExtractor(varargin)
+      % Set to true when extractDescriptors implemented
+      obj.ExtractsDescriptors = true;
+      obj.Name = ''; % Name of the wrapper
+      obj.DetectorName = ''; % Name of feature detection algorithm
+      obj.DescriptorName = ''; % Name of descriptor calc. algorithm
+
+      % Parse class options
+      obj.Opts = vl_argparse(obj.Opts,varargin);
+
+      % Other constructor stuff...
+    end
+
+    function [frames descriptors] = extractFeatures(obj, imagePath)
+      % Try to load features from cache
+      [frames descriptors] = obj.loadFeatures(imagePath,nargout > 1);
+      if numel(frames) > 0; return; end;
+
+      frames = []; descriptors = [];
+      if nargout == 1
+        obj.info('Computing frames of image %s.',getFileName(imagePath));
+        % Code for feature frame detection without descriptors
+      else
+        obj.info('Computing frames and descriptors of image %s.',...
+          getFileName(imagePath));
+        % Code for feature frame detection plus their descriptors. 
+      end
+
+      % Store features to cache
+      obj.storeFeatures(imagePath, frames, descriptors);
+    end
+
+    function [frames descriptors] = extractDescriptors(obj, imagePath, frames)
+      % Code to extract descriptors of given frames
+      descriptors = [];
+    end
+
+    function signature = getSignature(obj)
+      % Code for generation of detector unique signature
+      signature = [helpers.struct2str(obj.Opts),';',...
+        helpers.fileSignature(mfilename('fullpath'))];
+    end
+  end
+end
